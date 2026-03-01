dd8cd01abfd5e2112f0ec2eb0450c9b9ecf629bf
Alex kumar <gk540807@gmail.com>
2026-03-01 19:59:08 +0530

    Add files via upload

---
+++ stories/s06.js
@@ -0,0 +1,5 @@
+stories.push({
+    title: "Story 5",
+    thumbnail: "https://static.wixstatic.com/media/daf650_100844930ba14a5f8a901283413a897f~mv2.gif",
+    episodes: [{ name: "Coming Soon", src: "" }]
+  });
